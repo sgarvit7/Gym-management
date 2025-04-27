fb00e50da358e2e77381aadb0098cf19b2190485
sgarvit7 <ssgarvit7@gmail.com>
2025-04-27 12:01:16 +0530

    responsive changes

--- gym-frontend/src/pages/GeneralUser/GeneralUser.jsx
+++ gym-frontend/src/pages/GeneralUser/GeneralUser.jsx
@@ -4,82 +4,81 @@ import { useEffect, useState } from "react";
 import { expired, expiringSoon, inActive, monthlyJoined, threeDays } from "./data";
 import { ToastContainer } from "react-toastify";
 
-const GeneralUser = ()=>{
+const GeneralUser = () => {
+  const [header, setHeader] = useState("");
+  const [data, setData] = useState([]);
 
-    const [header,setHeader] = useState("");
-    const [data,setData] = useState([]);
-    useEffect(()=>{
-        const func = sessionStorage.getItem('func');
-        functionCall(func);
-    },[])
-    const functionCall = async(func)=>{
-            switch (func){
-                case "Monthly":
-                  const datas = await monthlyJoined();
-                  setData(datas);
+  useEffect(() => {
+    const func = sessionStorage.getItem('func');
+    functionCall(func);
+  }, []);
 
-                    setHeader("Monthly joined member")
-                    break;
-                case "3days":
-                  const datatd = await threeDays();
-                  setData(datatd);
-                    setHeader("Expiring in 3 days")
-                    break;
-                    
-                case "expiringSoon":
-                  const dataes = await expiringSoon();
-                  setData(dataes);
-                    setHeader("Expiring Soon")
-                    break;
-                    
-                 case "Expired":
-                  const datae = await expired();
-                  setData(datae);
-                    setHeader("Expired")
-                    break;
-                    
-                    case "Inactive":
-                      const datain = await inActive();
-                      setData(datain);
-                        setHeader("InActive")
-                        break;
-            }
+  const functionCall = async (func) => {
+    try {
+      switch (func) {
+        case "Monthly":
+          const datas = await monthlyJoined();
+          setData(datas);
+          setHeader("Monthly joined members");
+          break;
+        case "3days":
+          const datatd = await threeDays();
+          setData(datatd);
+          setHeader("Expiring in 3 days");
+          break;
+        case "expiringSoon":
+          const dataes = await expiringSoon();
+          setData(dataes);
+          setHeader("Expiring Soon");
+          break;
+        case "Expired":
+          const datae = await expired();
+          setData(datae);
+          setHeader("Expired");
+          break;
+        case "Inactive":
+          const datain = await inActive();
+          setData(datain);
+          setHeader("Inactive");
+          break;
+        default:
+          setHeader("Members");
+          setData([]);
+      }
+    } catch (error) {
+      console.error("Error fetching data:", error);
     }
+  };
 
-    return(
- <div className="text-black  h-[100vh]">
-      {/* block for banner */}
-      <div className="border-2 bg-slate-900 flex justify-between w-full text-white rounded-lg p-3">
-        <Link to={'/Dashboard'}
-          className="border-2 pl-3 pr-3 pt-1 rounded-2xl flex cursor-pointer hover:bg-white  hover:text-black"
+  return (
+    <div className="text-black min-h-screen p-4 bg-white">
+      {/* Top Banner */}
+      <div className="border-2 bg-slate-900 flex flex-col sm:flex-row justify-between items-center w-full text-white rounded-lg p-3 gap-3">
+        <Link
+          to={'/Dashboard'}
+          className="border-2 pl-4 pr-4 pt-2 pb-2 rounded-2xl flex items-center cursor-pointer hover:bg-white hover:text-black transition"
         >
           Back to Dashboard
-          {/* <img className="w-8 h-8" src={dumbell} /> */}
-        </Link>   
+        </Link>
       </div>
-      <div className="mt-5 ml-5 text-xl text-slate-900">
+
+      {/* Header */}
+      <div className="mt-6 ml-2 text-2xl font-semibold text-slate-800">
         {header}
       </div>
-       <div>
-      <div className="bg-slate-100 p-5 mt-5 rounded-lg grid gap-2 grid-cols-3 overflow-x-auto h-[65%]">
 
-        
-       {
-          data.map((item,index)=>{
-            return(
-                <MemberCard  key={item._id || index} item={item}/>
-            )
-            
-}
-       ) }
-
+      {/* Cards Section */}
+      <div className="bg-slate-100 p-5 mt-5 rounded-lg grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 overflow-auto">
+        {
+          data.map((item, index) => (
+            <MemberCard key={item._id || index} item={item} />
+          ))
+        }
       </div>
-     </div>
 
-
-    <ToastContainer/>        
+      <ToastContainer />
     </div>
-    
-    )
-}
+  );
+};
+
 export default GeneralUser;
